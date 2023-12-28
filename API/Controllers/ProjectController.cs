734f46b5b8d2e2389cc08cdefd9864847ba891e5
mahadi025 <mahadikarimmunif@gmail.com>
2023-12-28 19:01:17 +0600

    Initial Commit

---
+++ API/Controllers/ProjectController.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public ProjectController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+    {
+        var Projects = await _context.Projects.ToListAsync();
+
+        return Projects;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Project>> GetProject(int id)
+    {
+        return await _context.Projects.FindAsync(id);
+    }
+}
---
+++ API/Entities/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+}
