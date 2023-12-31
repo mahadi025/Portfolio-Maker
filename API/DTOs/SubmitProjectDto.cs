e384f16ddcf5bbd7dbade827ea9e337744d3cb86
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-01 00:02:54 +0600

    added SubmitProjectDto

---
+++ API/DTOs/SubmitProjectDto.cs
@@ -0,0 +1,9 @@
+namespace API.DTOs;
+
+public class SubmitProjectDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Url { get; set; }
+}
