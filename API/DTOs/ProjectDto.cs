ea67aefaf93c61990fba1b5f2701e156664e4232
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-01 00:01:41 +0600

    removed id

--- API/DTOs/ProjectDto.cs
+++ API/DTOs/ProjectDto.cs
@@ -4,7 +4,6 @@ namespace API.DTOs;
 
 public class ProjectDto
 {
-    public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Url { get; set; }
