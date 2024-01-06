b997d301c5593abdc33f167e4e7cb3ea0e28afdc
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-06 19:32:30 +0600

    moved the interfaces

---
+++ API/Interfaces/ISkillRepository.cs
@@ -0,0 +1,10 @@
+﻿using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ISkillRepository
+{
+    Task<bool> SaveAllAsync();
+    Task<IEnumerable<Skill>> GetSkillsAsync();
+    // public Task<List<Skill>> GetSkillsByProjectNameAsync(string projectName);
+}
