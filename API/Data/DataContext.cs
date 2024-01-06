91c402cf6d7d0aa649f29a89bfabb5bc53a4e0eb
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-06 19:32:00 +0600

    added Skill to the database

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,4 +11,5 @@ public class DataContext : DbContext
 
     public DbSet<AppUser> Users { get; set; }
     public DbSet<Project> Projects { get; set; }
+    public DbSet<Skill> Skills { get; set; }
 }
