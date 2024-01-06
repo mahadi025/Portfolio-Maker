de504fe27c22d7ef408d97d825154ce79ec6dba1
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-06 19:33:21 +0600

    added skill repository

---
+++ API/Data/SkillRepository.cs
@@ -0,0 +1,33 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class SkillRepository : ISkillRepository
+{
+    private readonly DataContext _context;
+
+    public SkillRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Skill>> GetSkillsAsync()
+    {
+        return await _context.Skills.ToListAsync();
+    }
+
+    // public async Task<List<Skill>> GetSkillsByProjectNameAsync(string projectName)
+    // {
+    //     return await _context.Skills
+    //     .Where(x => x.Project.Name == projectName)
+    //     .ToListAsync();
+    // }
+
+    public async Task<bool> SaveAllAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+}
