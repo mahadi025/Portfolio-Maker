9d338dcb3921d09bc8670ab89860217a2f719347
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-08 18:15:19 +0600

    added client

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    https: {
+      key: './ssl/localhost-key.pem',
+      cert: './ssl/localhost.pem'
+    },
+    port: 3000
+  },
+
+  plugins: [react()],
+})
