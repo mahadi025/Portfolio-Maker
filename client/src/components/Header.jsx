9edc13c57dc56dd7adc4fc0d80d40b99749850fc
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-13 18:39:29 +0600

    implemented responsiveness to the navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,25 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import '../styles/header.css'
 
 function Header(props) {
 
+    const [showNavbar, setShowNavbar] = useState(false);
+
+    const handleMenuClick = () => {
+        setShowNavbar(!showNavbar);
+    };
+
     return (
         <header className="header">
-            <Link to="/" className="logo">Portfolio</Link>
-            <i className='bx bx-menu' id="menu-icon"></i>
-            <nav className="navbar">
-                <Link to="/" className="active">Home</Link>
+            <Link to="/" className="logo">
+                Portfolio
+            </Link>
+            <i className="bx bx-menu" id="menu-icon" onClick={handleMenuClick}></i>
+            <nav className={`navbar ${showNavbar ? 'show' : ''}`}>
+                <Link to="/" className="active">
+                    Home
+                </Link>
                 <Link to="/about">About</Link>
                 <Link to="/skill">Skills</Link>
                 <Link to="/project">Projects</Link>
@@ -16,7 +27,11 @@ function Header(props) {
                 <button className="theme-btn" id="theme-btn" onClick={props.handleThemeToggle}>
                     <i className={`bx bx-adjust`}></i>
                 </button>
-                {props.user != null && <button className="logout-btn" onClick={props.handleLogout}>Logout</button>}
+                {props.user != null && (
+                    <button className="logout-btn" onClick={props.handleLogout}>
+                        Logout
+                    </button>
+                )}
             </nav>
         </header>
     );
