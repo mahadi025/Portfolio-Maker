51b27c78e27ff9e7003fcb13b07f77abf30749e7
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-14 23:18:28 +0600

    added bootstrap card

--- client/src/components/SkillList.jsx
+++ client/src/components/SkillList.jsx
@@ -3,67 +3,94 @@ import '../styles/skills.css'
 
 function SkillList() {
     return (
-        <section className="skills" id="skills">
-            <h2 className="heading">My <span>Skills</span></h2>
-            <div className="skills-container">
-                <div className="skills-box">
-                    <i className='bx bxl-python'></i>
-                    <h3>Python</h3>
+        <div className="container skill">
+            <div className="row">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Python</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-python'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bx-hash'></i>
-                    <h3>C Sharp</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">C#</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bx-hash'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-java'></i>
-                    <h3>Java</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Java</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-java'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-c-plus-plus'></i>
-                    <h3>C++</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">C++</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-c-plus-plus'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-javascript'></i>
-                    <h3>JS</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">JavaScript</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-javascript'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bx-laptop'></i>
-                    <h3>Machine Learning</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Machine Learning</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bx-laptop'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bx-images'></i>
-                    <h3>Image Processing</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Image Processing</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bx-images'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-django'></i>
-                    <h3>Django</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Django</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-django'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-react'></i>
-                    <h3>React</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">React</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-react'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-angular'></i>
-                    <h3>Angular</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Angular</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-angular'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-flask'></i>
-                    <h3>Flask</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Flask</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-flask'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-mongodb'></i>
-                    <h3>MongoDB</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">MongoDB</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-mongodb'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-github'></i>
-                    <h3>Git</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Git</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-github'></i></h6>
+                    </div>
                 </div>
-                <div className="skills-box">
-                    <i className='bx bxl-docker'></i>
-                    <h3>Docker</h3>
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-title">Docker</h5>
+                        <h6 className="card-subtitle mb-2 text-muted"><i className='bx bxl-docker'></i></h6>
+                    </div>
                 </div>
             </div>
-        </section>
+        </div>
     );
 }
 
