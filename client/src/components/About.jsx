739f0a73e785b57beb673e72e7ee6eba15fa7745
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-15 17:20:15 +0600

    implemented responsiveness to the about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
 
     useEffect(() => {
         const typed2 = new Typed(".multiple-text2", {
-            strings: ["Software Developer", ""],
+            strings: ["Web Developer"],
             typeSpeed: 100,
             backSpeed: 100,
             typeDelay: 1000,
@@ -31,28 +31,53 @@ function About() {
 
 
     return (
-        <section className="about" id="about">
-            <div className="about-img">
-                <img src={profilePic} alt="" />
+        <div className="container about-container">
+            <div className="row d-flex align-items-center justify-content-center">
+                <div className="col-4 about-img">
+                    <img className="rounded-circle" src={profilePic} alt="" />
+                </div>
+                <div className="col-8 about-content">
+                    <h2 className="heading">About <span>ME</span></h2>
+                    <h3><span className="multiple-text2"></span></h3>
+                    <p>Hello, My name is Mahadi Karim Munif. I graduated from East West University with BSC in Computer Science
+                        Engineering. I love programming. I
+                        know <span className="badge badge-pill badge-success"><i>C</i></span>, <span className="badge badge-pill badge-success"><i>C++</i></span>, <span
+                            className="badge badge-pill badge-success"><i>JAVA</i></span>, <span className="badge badge-pill badge-success"><i>Python</i></span>,
+                        <span className="badge badge-pill badge-success"><i>C#</i></span>, <span className="badge badge-pill badge-success"><i>JavaScript</i></span>.
+                        I like to build different kinds of Android
+                        apps using <span className="badge badge-pill badge-info"><b>Android Studio</b></span> and Websites using <span
+                            className="badge badge-pill badge-info"><b>Django</b></span>, <span className="badge badge-pill badge-info"><b>Flask</b></span>,
+                        and <span className="badge badge-pill badge-info"><b>ASP.NET</b></span> I
+                        also have experience in data science, machine learning, and image processing.
+                        <br />
+                        I like to learn from <span className="multiple-text3"></span>
+                    </p>
+                </div>
             </div>
-            <div className="about-content">
-                <h2 className="heading">About <span>ME</span></h2>
-                <h3><span className="multiple-text2"></span></h3>
-                <p>Hello, My name is Mahadi Karim Munif. I graduated from East West University with BSC in Computer Science
-                    Engineering. I love programming. I
-                    know <span className="multiple-text2"><b>C</b></span>, <span className="multiple-text2"><b>C++</b></span>, <span
-                        className="multiple-text2"><b>JAVA</b></span>, <span className="multiple-text2"><b>Python</b></span>,
-                    <span className="multiple-text2"><b>C#</b></span>, <span className="multiple-text2"><b>JavaScript</b></span>.
-                    I like to build different kinds of Android
-                    apps using <span className="multiple-text2"><b>Android Studio</b></span> and Websites using <span
-                        className="multiple-text2"><b>Django</b></span> and <span className="multiple-text2"><b>Flask</b></span>,
-                    and <span className="multiple-text2"><b>ASP.NET</b></span> I
-                    also have experience in data science, machine learning, and image processing.
-                    <br />
-                    I like to learn from <span className="multiple-text3"></span>
-                </p>
-            </div>
-        </section>
+        </div>
+
+        // <section className="about" id="about">
+        //     <div className="about-img">
+        //         <img src={profilePic} alt="" />
+        //     </div>
+        //     <div className="about-content">
+        //         <h2 className="heading">About <span>ME</span></h2>
+        //         <h3><span className="multiple-text2"></span></h3>
+        //         <p>Hello, My name is Mahadi Karim Munif. I graduated from East West University with BSC in Computer Science
+        //             Engineering. I love programming. I
+        //             know <span><i>C</i></span>, <span><i>C++</i></span>, <span><i>JAVA</i></span>,
+        //             <span><i>Python</i></span>,
+        //             <span><i>C#</i></span>, <span><i>JavaScript</i></span>.
+        //             I like to build different kinds of Android
+        //             apps using <span><b>Android Studio</b></span> and Websites using <span><b>Django</b></span> and
+        //             <span><b>Flask</b></span>,
+        //             and <span><b>ASP.NET</b></span> I
+        //             also have experience in data science, machine learning, and image processing.
+        //             <br />
+        //             I like to learn from <span className="multiple-text3"></span>
+        //         </p>
+        //     </div>
+        // </section>
     );
 }
 
