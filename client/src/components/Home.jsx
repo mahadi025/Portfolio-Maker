9ba0ba621866ed634f57d11a1a3ef8f03221f4ef
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-15 17:20:53 +0600

    added frontend and backend

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,37 +10,54 @@ function Home() {
 
     useEffect(() => {
         const typed1 = new Typed(".multiple-text1", {
-            strings: ["Software Developer", ""],
+            strings: ["React", "Angular"],
+            typeSpeed: 100,
+            backSpeed: 100,
+            typeDelay: 1000,
+            loop: true
+        });
+        const typed2 = new Typed(".multiple-text2", {
+            strings: ["ASP.NET", "Django", "Flask"],
             typeSpeed: 100,
             backSpeed: 100,
             typeDelay: 1000,
             loop: true
         });
 
+
         return () => {
             typed1.destroy();
+            typed2.destroy();
         };
     }, []);
 
 
     return (
-        <section className="home" id="home">
-            <div className="home-content">
-                <h3>Hi, I am</h3>
-                <h3>Mahadi Karim <span>Munif</span></h3>
-                <h3>and I am a <span className="multiple-text1"></span></h3>
-                <p>“You are never too old to set another goal or to dream a new dream.” ~ C.S Lewis</p>
-                <div className="social-media">
-                    <a href="https://www.linkedin.com/in/mahadi025/" target="_blank" rel="noreferrer"><i className="bx bxl-linkedin"></i></a>
-                    <a href="https://www.facebook.com/MahadiKarimMunif" target="_blank" rel="noreferrer"><i className="bx bxl-facebook"></i></a>
-                    <a href="https://github.com/mahadi025" target="_blank" rel="noreferrer"><i className="bx bxl-github"></i></a>
+        <div className="container">
+            <div className="home row">
+                <div className="col-8">
+                    <h3>Hi, I am</h3>
+                    <h3>Mahadi Karim <span>Munif</span></h3>
+                    <h3>and I am a <span> Web Developer</span></h3>
+                    <h3>Frontend <span className="multiple-text1"></span></h3>
+                    <h3>Backend <span className="multiple-text2"></span></h3>
+                    <p>“You are never too old to set another goal or to dream a new dream.” ~ C.S Lewis</p>
+                    <div className="social-media">
+                        <a href="https://www.linkedin.com/in/mahadi025/" target="_blank" rel="noreferrer"><i className="bx bxl-linkedin"></i></a>
+                        <a href="https://www.facebook.com/MahadiKarimMunif" target="_blank" rel="noreferrer"><i className="bx bxl-facebook"></i></a>
+                        <a href="https://github.com/mahadi025" target="_blank" rel="noreferrer"><i className="bx bxl-github"></i></a>
+                    </div>
+                    <div className="row mt-2">
+                        <div className="col">
+                            <a href={pdf} target="_blank" className="resume-btn" rel="noreferrer">Download Resume</a>
+                        </div>
+                    </div>
                 </div>
-                <a href={pdf} target="_blank" className="resume-btn" rel="noreferrer">Download Resume</a>
+                <div className="col-4">
+                    <img className="home-img" src={profilePic} alt="" />
+                </div>
             </div>
-            <div className="home-img">
-                <img src={profilePic} alt="" />
-            </div>
-        </section>
+        </div>
     );
 }
 
