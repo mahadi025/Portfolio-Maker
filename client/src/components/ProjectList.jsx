001a1f82b54554b656a0426cba8b76ad2ae35c75
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-15 16:25:10 +0600

    added heading

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -9,17 +9,20 @@ function ProjectList() {
     const { data: projects, loading, error } = useFetch('https://localhost:5001/api/Project');
 
     return (
-        <div className="project-container">
-            {projects.map(project => (
-                <Link to={`/project/${project.id}`} key={project.id}>
-                    <div className="project-box">
-                        <img src={projectPic} alt="" />
-                        <div className="project-layer">
-                            <h4>Advising Portal</h4>
+        <div className="project">
+            <h2 class="heading">My <span>Projects</span></h2>
+            <div className="project-container">
+                {projects.map(project => (
+                    <Link to={`/project/${project.id}`} key={project.id}>
+                        <div className="project-box">
+                            <img src={projectPic} alt="" />
+                            <div className="project-layer">
+                                <h4>Advising Portal</h4>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            ))}
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 }
