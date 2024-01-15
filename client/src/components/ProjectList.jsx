ace524c2b47c06212431cd035e7bb99b3c7dcc6f
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-15 16:22:19 +0600

    implemented responsive project page

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -1,29 +1,26 @@
 import 'boxicons/css/boxicons.min.css';
 import projectPic from "../assets/project.jpg"
 import useFetch from '../useFetch';
-import { Link } from 'react-router-dom';
 import '../styles/projectlist.css'
+
+import { Link } from 'react-router-dom';
 
 function ProjectList() {
     const { data: projects, loading, error } = useFetch('https://localhost:5001/api/Project');
 
     return (
-        <section className="project" id="project">
-            <h2 className="heading">My <span>Projects</span></h2>
-            <div className="project-container">
-                {projects.map(project => (
-                    <div className="project-box" key={project.id}>
+        <div className="project-container">
+            {projects.map(project => (
+                <Link to={`/project/${project.id}`} key={project.id}>
+                    <div className="project-box">
                         <img src={projectPic} alt="" />
-                        <Link to={`/project/${project.id}`}>
-                            <div className="project-layer">
-                                <h4>{project.name}</h4>
-                            </div>
-                        </Link>
+                        <div className="project-layer">
+                            <h4>Advising Portal</h4>
+                        </div>
                     </div>
-                ))}
-            </div>
-        </section>
+                </Link>
+            ))}
+        </div>
     );
 }
-
 export default ProjectList;
