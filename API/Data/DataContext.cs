f67a7617a457457d0670547b06e1c5259ea7bce2
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-16 14:24:22 +0600

    added IdentityDbContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,15 +1,18 @@
 ﻿using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext<AppUser, IdentityRole<int>, int,
+    IdentityUserClaim<int>, IdentityUserRole<int>, IdentityUserLogin<int>,
+    IdentityRoleClaim<int>, IdentityUserToken<int>>
 {
-    public DataContext(DbContextOptions options) : base(options)
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
     }
 
-    public DbSet<AppUser> Users { get; set; }
     public DbSet<Project> Projects { get; set; }
     public DbSet<Skill> Skills { get; set; }
 }
