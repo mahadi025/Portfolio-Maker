9bfa11cc5bff0fa920e35501eec06e35a8a72a77
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-19 21:54:32 +0600

    added FirstName and LastName

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -6,6 +6,8 @@ public class MemberDto
 {
     public int Id { get; set; }
     public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string PhotoUrl { get; set; }
     public int Age { get; set; }
     public string KnownAs { get; set; }
