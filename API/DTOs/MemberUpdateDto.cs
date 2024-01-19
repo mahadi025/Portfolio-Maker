26677dba331766a9ab97cf57766cf60be74d575f
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-19 21:54:49 +0600

    added FirstName, LastName, and Gender

--- API/DTOs/MemberUpdateDto.cs
+++ API/DTOs/MemberUpdateDto.cs
@@ -2,6 +2,10 @@
 
 public class MemberUpdateDto
 {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string KnownAs { get; set; }
+    public string Gender { get; set; }
     public int Age { get; set; }
     public string Introduction { get; set; }
     public string City { get; set; }
