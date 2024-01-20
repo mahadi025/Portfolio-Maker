f1c8a263abf4f0c535beefe02ba214a5e17a1323
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-20 23:02:44 +0600

    added logged in user check

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "../axiosConfig";
 import '../styles/login.css'
 import { useNavigate } from 'react-router-dom';
+import { getLoggedInUser } from "../auth";
 
 function Login() {
     const navigateTo = useNavigate();
@@ -10,11 +11,20 @@ function Login() {
 
     const [password, setPassword] = useState('');
 
+    const user = getLoggedInUser();
+
+
+    useEffect(() => {
+        if (user) {
+            navigateTo("/");
+        }
+    }, [])
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('https://localhost:5001/api/account/login/', {
+            const response = await axios.post('/account/login/', {
                 username: username,
                 password: password,
             });
@@ -28,14 +38,14 @@ function Login() {
     };
 
     return (
-        <section className="login-form-container">
+        <div className="login-form-container">
             <h2 className="heading"><span>Login</span></h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" type="text" id="username" name="username" />
                 <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" id="password" name="password" />
                 <button className="login-btn" type="submit">Log In</button>
             </form>
-        </section>
+        </div>
     )
 }
 
