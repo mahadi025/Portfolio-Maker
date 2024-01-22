54ee4bd80405b8b72d2d740e0c54581fa4baeb56
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-22 21:34:09 +0600

    implemented getLoggedInUser

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,8 +3,13 @@ import '../styles/header.css'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { getLoggedInUser } from "../auth";
 
 function Header(props) {
+
+    const user = getLoggedInUser();
+
+
     return (
         <Navbar expand="lg" className="header bg-body-tertiary">
             <Container id="nav">
@@ -15,9 +20,9 @@ function Header(props) {
                         <Nav.Link as={Link} to="/about" className="nav-link">About</Nav.Link>
                         <Nav.Link as={Link} to="/skill" className="nav-link">Skills</Nav.Link>
                         <Nav.Link as={Link} to="/project" className="nav-link">Projects</Nav.Link>
-                        {props.user && <Nav.Link as={Link} to="/create-project" className="nav-link">New Project +</Nav.Link>}
-                        {props.user && <Nav.Link as={Link} to="/profile" className="nav-link">Profile</Nav.Link>}
-                        {props.user != null && (
+                        {user && <Nav.Link as={Link} to="/create-project" className="nav-link">New Project +</Nav.Link>}
+                        {user && <Nav.Link as={Link} to="/profile" className="nav-link">Profile</Nav.Link>}
+                        {user && (
                             <button className="logout-btn" onClick={props.handleLogout}>
                                 <i className='bx bx-log-out' ></i>
                             </button>
