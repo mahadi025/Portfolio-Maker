f6e36b01c87b76d1b409c407325e60ba498290f7
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-22 21:32:54 +0600

    updated about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,15 +8,15 @@ function About(props) {
     const user = props.mainUser;
 
     useEffect(() => {
-        const typed2 = new Typed(".multiple-text2", {
-            strings: [user.introduction],
+        const typed1 = new Typed(".multiple-text1", {
+            strings: [user.introduction || ""],
             typeSpeed: 100,
             backSpeed: 100,
             typeDelay: 1000,
             loop: true
         });
 
-        const typed3 = new Typed(".multiple-text3", {
+        const typed2 = new Typed(".multiple-text2", {
             strings: ["anything", "and everything."],
             typeSpeed: 80,
             backSpeed: 80,
@@ -25,34 +25,32 @@ function About(props) {
         });
 
         return () => {
+            typed1.destroy();
             typed2.destroy();
-            typed3.destroy();
         };
-    }, []);
+    }, [user]);
 
     return (
-        <div className="container about-container">
-            <div className="d-flex align-items-center justify-content-center">
-                <div className="about-img">
-                    <img className="rounded-circle" src={user.photoUrl} alt="" />
-                </div>
-                <div className="about-content pl-2">
-                    <h2 className="heading">About <span>ME</span></h2>
-                    <h3><span className="multiple-text2"></span></h3>
-                    <p>Hello, My name is {user.firstName} {user.lastName}. I graduated from East West University with BSC in Computer Science
-                        Engineering. I love programming. I
-                        know <span className="badge badge-pill badge-success"><i>C</i></span>, <span className="badge badge-pill badge-success"><i>C++</i></span>, <span
-                            className="badge badge-pill badge-success"><i>JAVA</i></span>, <span className="badge badge-pill badge-success"><i>Python</i></span>,
-                        <span className="badge badge-pill badge-success"><i>C#</i></span>, <span className="badge badge-pill badge-success"><i>JavaScript</i></span>.
-                        I like to build different kinds of Android
-                        apps using <span className="badge badge-pill badge-info"><b>Android Studio</b></span> and Websites using <span
-                            className="badge badge-pill badge-info"><b>Django</b></span>, <span className="badge badge-pill badge-info"><b>Flask</b></span>,
-                        and <span className="badge badge-pill badge-info"><b>ASP.NET</b></span> I
-                        also have experience in data science, machine learning, and image processing.
-                        <br />
-                        I like to learn from <span className="multiple-text3"></span>
-                    </p>
-                </div>
+        <div className="about">
+            <div className="about-img">
+                <img src={user.photoUrl} alt="" />
+            </div>
+            <div className="about-content">
+                <h2 className="heading">About <span>ME</span></h2>
+                <h3><span className="multiple-text1"></span></h3>
+                <p>Hello, My name is {user.firstName} {user.lastName}. I graduated from East West University with BSC in Computer Science
+                    Engineering. I love programming. I
+                    know <span><i>C</i></span>, <span><i>C++</i></span>, <span><i>JAVA</i></span>,
+                    <span><i>Python</i></span>,
+                    <span><i>C#</i></span>, <span><i>JavaScript</i></span>.
+                    I like to build different kinds of Android
+                    apps using <span><b>Android Studio</b></span> and Websites using <span><b>Django, </b></span>
+                    <span><b>Flask</b></span>,
+                    and <span><b>ASP.NET</b></span> I
+                    also have experience in data science, machine learning, and image processing.
+                    <br />
+                    I like to learn from <span className="multiple-text2"></span>
+                </p>
             </div>
         </div>
     );
