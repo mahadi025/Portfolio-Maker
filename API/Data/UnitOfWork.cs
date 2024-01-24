8369ba257941f6f4fffa6763a2606e7c15ec7f43
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-24 12:53:37 +0600

    implemented IUnitOfWork

---
+++ API/Data/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Interfaces;
+
+namespace API;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DataContext _context;
+
+    public UnitOfWork(DataContext context)
+    {
+        _context = context;
+    }
+    public IUserRepository UserRepository => new UserRepository(_context);
+
+    public IProjectRepository ProjectRepository => new ProjectRepository(_context);
+
+    public ISkillRepository SkillRepository => new SkillRepository(_context);
+
+    public async Task<bool> Complete()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+    public bool HasChanges()
+    {
+        return _context.ChangeTracker.HasChanges();
+    }
+
+}
