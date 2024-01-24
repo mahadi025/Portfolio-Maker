b1363ca392c2a0974d85ec318ec874929814d7f5
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-24 12:53:14 +0600

    added IUnitOfWork

---
+++ API/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using API.Interfaces;
+
+namespace API;
+
+public interface IUnitOfWork
+{
+    IUserRepository UserRepository { get; }
+    IProjectRepository ProjectRepository { get; }
+    ISkillRepository SkillRepository { get; }
+    Task<bool> Complete();
+    bool HasChanges();
+}
