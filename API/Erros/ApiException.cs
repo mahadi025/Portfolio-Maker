cc7ebd259662319ba97b439b6a98017571106b62
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-25 15:18:02 +0600

    added ApiException

---
+++ API/Erros/ApiException.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Errors;
+
+public class ApiException
+{
+    public ApiException(int statusCode, string message, string details)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Details = details;
+    }
+
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public string Details { get; set; }
+
+    public static implicit operator CreatedAtActionResult(ApiException v)
+    {
+        throw new NotImplementedException();
+    }
+}
